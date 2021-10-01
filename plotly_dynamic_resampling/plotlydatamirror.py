db3c2ccc7e069a37aca1aa42299c936f3d0a6b31
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2021-10-01 11:15:44 +0200

    :zap: now supports subplot-based resampling :goat:

--- plotly_dynamic_resampling/plotlydatamirror.py
+++ plotly_dynamic_resampling/plotlydatamirror.py
@@ -2,10 +2,14 @@
 """
 Wrapper around the plotly figure to allow bookkeeping and back-end based resampling of
 HF data.
+
+Future work:
+    * Add functionality to let the user define a downsampling method
+
 """
 __author__ = "Jonas Van Der Donckt, Emiel Deprost"
 
-from typing import List, Optional
+from typing import List, Optional, Union, Iterable
 
 import pandas as pd
 import plotly.graph_objects as go
@@ -15,6 +19,7 @@ import dash_core_components as dcc
 from jupyter_dash import JupyterDash
 from dash.dependencies import Input, Output, State
 from uuid import uuid4
+import re
 
 
 class PlotlyDataMirror(go.Figure):
@@ -41,14 +46,12 @@ class PlotlyDataMirror(go.Figure):
         self._hf_data: List[dict] = []
         self._global_n_shown_samples = global_n_shown_samples
         self._print_verbose = verbose
-
-        # downsampling method, optional
-        self._downsampler = None
+        self._downsampler = None  # downsampling method, still to be implemented
 
         super().__init__(figure)
 
     def _query_hf_data(self, trace: dict) -> Optional[dict]:
-        """Query the internal `hf_data` attribute and returns a match.
+        """Query the internal `hf_data` attribute and returns a match based on `uid`.
 
         Parameters
         ----------
@@ -69,16 +72,11 @@ class PlotlyDataMirror(go.Figure):
             k: trace[k]
             for k in set(trace.keys()).difference({'x', 'y'})
         }
-        print(f"[W] trace with {trace_props} not found")
+        self._print(f"[W] trace with {trace_props} not found")
         return None
 
-    def _get_hf_data_props(self) -> List[dict]:
-        return [
-            {k: hf[k] for k in set(hf.keys()).difference({'x', 'y'})}
-            for hf in self._hf_data
-        ]
-
     def _print(self, *values):
+        """Helper method for printing if `verbose` is set to True"""
         if self._print_verbose:
             print(*values)
 
@@ -112,20 +110,19 @@ class PlotlyDataMirror(go.Figure):
         """
         hf_data = self._query_hf_data(trace)
         if hf_data is not None:
-            df_res: pd.Series = self._resample_series(
-                df_data=hf_data["df_hf"],
-                max_n_samples=hf_data["max_n_samples"],
-                t_start=t_start,
-                t_stop=t_stop,
-            )
+            df_data = self._slice_time(hf_data['df_hf'], t_start, t_stop)
+            df_res: pd.Series = self._resample_series(df_data, hf_data["max_n_samples"])
             trace["x"] = df_res.index
             trace["y"] = df_res.values
+        else:
+            self._print('hf_data not found')
 
     def check_update_figure_dict(
             self,
             figure: dict,
             t_start: Optional[pd.Timestamp] = None,
             t_stop: Optional[pd.Timestamp] = None,
+            xaxis: str = None,
     ):
         """Check and update the traces within the figure dict.
 
@@ -147,62 +144,45 @@ class PlotlyDataMirror(go.Figure):
         t_stop : Optional[pd.Timestamp], optional
             The end time for which we want the resampled data to be updated to,
             by default None
-
+        xaxis: str, Optional
+            Additional trace-update filter
         """
         for trace in figure["data"]:
-            # print(trace.keys())
+            if xaxis is not None and trace.get('xaxis', None) != xaxis:
+                continue
             self.check_update_trace_data(trace=trace, t_start=t_start, t_stop=t_stop)
+
+    @staticmethod
+    def _slice_time(
+            df_data: pd.Series,
+            t_start: Optional[pd.Timestamp] = None,
+            t_stop: Optional[pd.Timestamp] = None,
+    ) -> pd.Series:
+        def to_same_tz(
+                ts: Union[pd.Timestamp, None],
+                reference_tz=df_data.index.tz
+        ) -> Union[pd.Timestamp, None]:
+            if ts is None:
+                return None
+            elif reference_tz is not None:
+                if ts.tz is not None:
+                    assert ts.tz.zone == reference_tz.zone
+                    return ts
+                else:  # localize -> time remains the same
+                    return ts.tz_localize(reference_tz)
+            elif reference_tz is None and ts.tz is not None:
+                return ts.tz_localize(None)
+
+        return df_data[to_same_tz(t_start):to_same_tz(t_stop)]
 
     @staticmethod
     def _resample_series(
             df_data: pd.Series,
-            max_n_samples: int = 6000,
-            t_start: Optional[pd.Timestamp] = None,
-            t_stop: Optional[pd.Timestamp] = None,
+            max_n_samples,
     ) -> pd.Series:
-        ts_tzone = df_data.index.tz
-        # todo -> maybe add ts_tzone
-
-        if isinstance(t_start, pd.Timestamp):
-            if ts_tzone is not None:
-                if t_start.tz is not None:
-                    assert t_start.tz.zone == ts_tzone.zone
-                else:  # localize -> time remains the same
-                    t_start = t_start.tz_localize(ts_tzone)
-            elif ts_tzone is None and t_start.tz is not None:
-                t_start = t_start.tz_localize(None)
-
-            df_data = df_data[t_start:]
-
-        if isinstance(t_stop, pd.Timestamp):
-            if ts_tzone is not None:
-                if t_stop.tz is not None:
-                    assert t_stop.tz.zone == ts_tzone.zone
-                else:  # localize -> time remains the same
-                    t_stop = t_stop.tz_localize(ts_tzone)
-            elif ts_tzone is None and t_stop.tz is not None:
-                t_stop = t_stop.tz_localize(None)
-
-            df_data = df_data[:t_stop]
-
-        # idx_series = df_data.index.to_series()
-        # t_start, t_stop = idx_series.iloc[0], idx_series.iloc[-1]
-        # timedelta_s = (t_stop - t_start).total_seconds()
-        # return df_data.resample(f"{(1e3*timedelta_s)//(1.5*max_n_samples)}ms").mean()
-        # todo -> check whether copy is necessary
-        df_res = df_data[:: (max(1, len(df_data) // max_n_samples))].copy()
-
+        df_res = df_data[:: (max(1, len(df_data) // max_n_samples))]
+        # ------- add None where there are gaps / irregularly sampled data
         tot_diff_sec_series = df_res.index.to_series().diff().dt.total_seconds()
-        # diff = t_curr-t_prev (no diff for first item)
-
-        # todo -> remove the max_gaps seconds param
-        # todo -> do we need to shift this with -1 (so the last one of the ..)
-        #   because we want to detect large gaps
-        #       -> validate
-        # if isinstance(max_gap_s, int):
-        #     # if ((t_stop - t_start).total_seconds() / len(df_res)) < max_gap_s:
-        #     if tot_diff_sec_series.median() < max_gap_s:
-        #         df_res.loc[tot_diff_sec_series > max_gap_s] = None
 
         # use a quantile based approach
         max_gap_q_s = tot_diff_sec_series.quantile(0.95)
@@ -220,14 +200,11 @@ class PlotlyDataMirror(go.Figure):
     def add_trace(
             self,
             trace,
-            row=None,
-            col=None,
-            secondary_y=None,
-            cut_points_to_view=False,
-            orig_x=None,  # Use this if you have high-dimensional data
-            orig_y=None,  # Use this if you have high-dimensional data
-            # resample_method="",
-            max_n_samples=None,
+            # Use this if you have high-dimensional data
+            orig_x: Optional[pd.DatetimeIndex] = None,
+            orig_y: Iterable = None,
+            max_n_samples: int = None,
+            cut_points_to_view: bool = False,
             **trace_kwargs
     ):
         """Add a trace to the figure.
@@ -267,31 +244,6 @@ class PlotlyDataMirror(go.Figure):
                     property then 'scatter' is assumed.
                   - All remaining properties are passed to the constructor
                     of the specified trace type.
-
-        row : 'all', int or None (default)
-            Subplot row index (starting from 1) for the trace to be
-            added. Only valid if figure was created using
-            `plotly.tools.make_subplots`.
-            If 'all', addresses all rows in the specified column(s).
-        col : 'all', int or None (default)
-            Subplot col index (starting from 1) for the trace to be
-            added. Only valid if figure was created using
-            `plotly.tools.make_subplots`.
-            If 'all', addresses all columns in the specified row(s).
-        secondary_y: boolean or None (default None)
-            If True, associate this trace with the secondary y-axis of the
-            subplot at the specified row and col. Only valid if all of the
-            following conditions are satisfied:
-              * The figure was created using `plotly.subplots.make_subplots`.
-              * The row and col arguments are not None
-              * The subplot at the specified row and col has type xy
-                (which is the default) and secondary_y True.  These
-                properties are specified in the specs argument to
-                make_subplots. See the make_subplots docstring for more info.
-              * The trace argument is a 2D cartesian trace
-                (scatter, bar, etc.)
-        cut_points_to_view: boolean
-            If True; the trace's datapoints will be add
         orig_x: pd.Series, optional
             The original high frequency time-index. If set, this has priority over the
             trace's data.
@@ -299,9 +251,17 @@ class PlotlyDataMirror(go.Figure):
             The original high frequency values. If set, this has priority over the
             trace's data.
         max_n_samples : int, optional
-            The maximum number of samples that will be shown by the trace.
+            The maximum number of samples that will be shown by the trace.\n
             .. note::
                 If this variable is not set; `_global_n_shown_samples` will be used.
+        cut_points_to_view: boolean, optional
+            If set to True and the trace it's format is a high-dimensional trace type,
+            then the trace's datapoints will be cut to the corresponding front-end view,
+            even if the total number of samples is lower than the MAX amount of samples.
+        **trace_kwargs:
+            Additional trace related keyword arguments
+            e.g.: row=.., col=..., secondary_y=...,
+            see trace_docs: https://plotly.com/python-api-reference/generated/plotly.graph_objects.Figure.html#plotly.graph_objects.Figure.add_traces
 
         Returns
         -------
@@ -312,22 +272,12 @@ class PlotlyDataMirror(go.Figure):
         if max_n_samples is None:
             max_n_samples = self._global_n_shown_samples
 
-        def super_add_trace(self):
-            return super().add_trace(
-                trace=trace,
-                row=row,
-                col=col,
-                secondary_y=secondary_y,
-                **trace_kwargs,
-            )
-
-        high_dimensional_traces = ["scatter", "scattergl"]
-
         # first add the trace, as each (even the non hf data traces), must contain this
         # key for comparison
         trace.uid = str(uuid4())
 
-        if trace["type"] in high_dimensional_traces:
+        high_dimensional_traces = ["scatter", "scattergl"]
+        if trace["type"].lower() in high_dimensional_traces:
             orig_x = trace["x"] if orig_x is None else orig_x
             orig_y = trace["y"] if orig_y is None else orig_y
 
@@ -335,46 +285,36 @@ class PlotlyDataMirror(go.Figure):
             assert len(orig_x) == len(orig_y)
 
             numb_samples = len(orig_x)
-            # these traces will determine the autoscale -> so also store when
-            # `cut_points_to_view` is set.
+            # these traces will determine the autoscale
+            #   -> so also store when cut_points_to_view` is set.
             if numb_samples > max_n_samples or cut_points_to_view:
                 self._print(
                     f"[i] resample {trace['name']} - {numb_samples}->{max_n_samples}")
 
-                # we will re-create this each time as df_hf wittholds
+                # we will re-create this each time as df_hf withholds
                 df_hf = pd.Series(data=orig_y, index=pd.to_datetime(orig_x), copy=False)
                 df_hf.rename('data', inplace=True)
                 df_hf.index.rename('timestamp', inplace=True)
 
                 # Checking this now avoids less interpretable `KeyError` when resampling
                 assert df_hf.index.is_monotonic_increasing
-
-                if self._downsampler is None:
-                    df_res = self._resample_series(
-                        df_hf, max_n_samples=max_n_samples
-                    )
-                    trace.x = df_res.index
-                    trace.y = df_res.values
-                else:
-                    raise NotImplementedError("Resampling method isn't supported!")
-
-                super_add_trace(self)
                 self._hf_data.append(
                     {
                         "max_n_samples": max_n_samples,
                         "df_hf": df_hf,
                         "uid": trace.uid
-
-                        # TODO -> add support for trace-based resampling methods
                         # "resample_method": "#resample_method,
                     }
                 )
+                # first resample the high-dim trace b4 converting it into javascript
+                self.check_update_trace_data(trace)
+                super().add_trace(trace=trace, **trace_kwargs)
             else:
                 self._print(
                     f"[i] NOT resampling {trace['name']} - {numb_samples} samples")
                 trace.x = orig_x
                 trace.y = orig_y
-                return super_add_trace(self)
+                return super().add_trace(trace=trace, **trace_kwargs)
         else:
             self._print(f"trace {trace['type']} is not a high-dimensional trace")
 
@@ -383,7 +323,7 @@ class PlotlyDataMirror(go.Figure):
             trace["y"] = trace["y"] if orig_y is not None else orig_y
             assert len(trace["x"]) > 0
             assert len(trace["x"] == len(trace["y"]))
-            return super_add_trace(self)
+            return super().add_trace(trace=trace, **trace_kwargs)
 
     def show_dash(self, mode=None, **kwargs):
         app = JupyterDash("local_app")
@@ -398,11 +338,30 @@ class PlotlyDataMirror(go.Figure):
             if changed_layout:
                 self._print("-" * 100 + "\n", "changed layout", changed_layout)
 
-                if "xaxis.range[0]" in changed_layout:
-                    t_start = pd.to_datetime(changed_layout["xaxis.range[0]"])
-                    t_stop = pd.to_datetime(changed_layout["xaxis.range[1]"])
-                    self.check_update_figure_dict(current_graph, t_start, t_stop)
-                elif changed_layout.get("xaxis.autorange", False):
+                # determine the start and end regex matches
+                def get_matches(regex: re.Pattern, strings: Iterable[str]) -> List[str]:
+                    matches = []
+                    for item in strings:
+                        m = regex.match(item)
+                        if m is not None:
+                            matches.append(m.string)
+                    return sorted(matches)
+
+                key_list = changed_layout.keys()
+                start_matches = get_matches(re.compile(r'xaxis\d*.range\[0]'), key_list)
+                stop_matches = get_matches(re.compile(r'xaxis\d*.range\[1]'), key_list)
+                if len(start_matches) and len(stop_matches):
+                    for t_start_key, t_stop_key in zip(start_matches, stop_matches):
+                        # check if the xaxis<NUMB> part of xaxis<NUMB>.[0-1] matches
+                        assert t_start_key.split('.')[0] == t_stop_key.split('.')[0]
+                        self.check_update_figure_dict(
+                            current_graph,
+                            t_start=pd.to_datetime(changed_layout[t_start_key]),
+                            t_stop=pd.to_datetime(changed_layout[t_stop_key]),
+                            xaxis='x' + t_start_key.split('.')[0][5:]
+                        )
+                elif len(get_matches(re.compile(r'xaxis\d*.autorange'), key_list)):
+                    # Autorange is applied on all axes -> hence no xaxis argument
                     self.check_update_figure_dict(current_graph)
             return current_graph
 
