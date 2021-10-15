ba024d9b2773fe72b14f5afc069b4c03bef7f1ef
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2021-10-15 15:16:19 +0200

    :sparkles: aggregationdownsampler now also works for sequential data

--- plotly_dynamic_resampling/downsamplers/downsamplers.py
+++ plotly_dynamic_resampling/downsamplers/downsamplers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Compatible implementation for various downsample methods
 """
-__author__ = 'Jonas Van Der Donckt'
+__author__ = "Jonas Van Der Donckt"
 
 from ..downsamplers.resampling_interface import AbstractSeriesDownsampler
 import pandas as pd
@@ -20,12 +20,12 @@ class LTTB(AbstractSeriesDownsampler):
     """
 
     def __init__(
-            self,
-            interleave_gaps: bool = True,
+        self,
+        interleave_gaps: bool = True,
     ):
         super().__init__(
             interleave_gaps,
-            dtype_regex_list=[rf"{dtype}\d*" for dtype in ['float', 'int', 'uint']]
+            dtype_regex_list=[rf"{dtype}\d*" for dtype in ["float", "int", "uint"]],
         )
 
     # TODO -> check whether these are able to deal with non-int based datatypes
@@ -35,7 +35,7 @@ class LTTB(AbstractSeriesDownsampler):
     # create a new C-file for which we support downsampling of categorical data
     # how often would this be useful?
     def _downsample(self, s: pd.Series, n_out: int) -> pd.Series:
-        idx, data = lttbc.downsample(np.arange(len(s), dtype='uint32'), s.values, n_out)
+        idx, data = lttbc.downsample(np.arange(len(s), dtype="uint32"), s.values, n_out)
         return pd.Series(index=s.iloc[idx.astype(int)].index, data=data, copy=False)
 
 
@@ -54,19 +54,26 @@ class AggregationDownsampler(AbstractSeriesDownsampler):
     """Downsampler method which uses the passed aggregation func."""
 
     def __init__(
-            self,
-            aggregation_func,
-            interleave_gaps: bool = True,
-            supported_dtypes=None
+        self, aggregation_func, interleave_gaps: bool = True, supported_dtypes=None
     ):
         self.aggregation_func = aggregation_func
         super().__init__(interleave_gaps, supported_dtypes)
 
     def _downsample(self, s: pd.Series, n_out: int) -> pd.Series:
         if isinstance(s.index, pd.DatetimeIndex):
-            t_start, t_end = s.index[::len(s) - 1]
+            t_start, t_end = s.index[:: len(s) - 1]
             rate = (t_end - t_start) / n_out
             return s.resample(rate).apply(self.aggregation_func).dropna()
 
-        rate = max(1, int(len(s) // n_out))
-        return s.resample(rate).apply(self.aggregation_func).dropna()
+        # no time index -> use the every nth heuristic
+        return (
+            s.groupby(
+                # create an array of [0, 0, 0, ...., n_out, n_out]
+                # where each value is repeated based $len(s)/n_out$ times
+                by=np.repeat(np.arange(n_out), max(1, np.ceil(len(s) / n_out)))[
+                    : len(s)
+                ]
+            )
+            .agg(self.aggregation_func)
+            .dropna()
+        )
