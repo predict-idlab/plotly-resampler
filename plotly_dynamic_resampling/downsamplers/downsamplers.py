d3c44e6726a0eba075a568acc0bb1b18edf05b1a
jonas <jonvdrdo.vanderdonckt@ugent.be>
2021-10-22 23:05:06 +0200

    :bug: fixing downsampler bugs

--- plotly_dynamic_resampling/downsamplers/downsamplers.py
+++ plotly_dynamic_resampling/downsamplers/downsamplers.py
@@ -3,11 +3,13 @@
 """
 __author__ = "Jonas Van Der Donckt"
 
-from ..downsamplers.downsampling_interface import AbstractSeriesDownsampler
-import pandas as pd
+import math
 
 import lttbc
 import numpy as np
+import pandas as pd
+
+from ..downsamplers.downsampling_interface import AbstractSeriesDownsampler
 
 
 class LTTB(AbstractSeriesDownsampler):
@@ -36,7 +38,12 @@ class LTTB(AbstractSeriesDownsampler):
     # how often would this be useful?
     def _downsample(self, s: pd.Series, n_out: int) -> pd.Series:
         idx, data = lttbc.downsample(np.arange(len(s), dtype="uint32"), s.values, n_out)
-        return pd.Series(index=s.iloc[idx.astype(int)].index, data=data, copy=False)
+        return pd.Series(
+            index=s.iloc[idx.astype(int)].index.astype(s.index.dtype),
+            data=data,
+            name=s.name,
+            copy=False
+        )
 
 
 class EveryNthPoint(AbstractSeriesDownsampler):
@@ -47,7 +54,7 @@ class EveryNthPoint(AbstractSeriesDownsampler):
         return True
 
     def _downsample(self, s: pd.Series, n_out: int) -> pd.Series:
-        return s[:: (max(1, len(s) // n_out))]
+        return s[:: max(1, math.ceil(len(s) / n_out))]
 
 
 class AggregationDownsampler(AbstractSeriesDownsampler):
@@ -66,14 +73,25 @@ class AggregationDownsampler(AbstractSeriesDownsampler):
             return s.resample(rate).apply(self.aggregation_func).dropna()
 
         # no time index -> use the every nth heuristic
-        return (
+        group_size = max(1, np.ceil(len(s) / n_out))
+        s_out = (
             s.groupby(
                 # create an array of [0, 0, 0, ...., n_out, n_out]
                 # where each value is repeated based $len(s)/n_out$ times
-                by=np.repeat(np.arange(n_out), max(1, np.ceil(len(s) / n_out)))[
-                    : len(s)
-                ]
+                by=np.repeat(np.arange(n_out), group_size)[: len(s)]
             )
             .agg(self.aggregation_func)
             .dropna()
+        )
+        # Create an index-estimation for real-time data
+        # Add one to the index so it's pointed at the end of the window
+        # Note: this can be adjusted to .5 to center the data
+        # multiply is with the croup size to get the real index-position
+        indec_locs = (np.arange(len(s_out)) + 1) * group_size
+        indec_locs[-1] = len(s) - 1
+        return pd.Series(
+            index=s.iloc[indec_locs.astype(s.index.dtype)].index.astype(s.index.dtype),
+            data=s_out.values,
+            name=s.name,
+            copy=False
         )
