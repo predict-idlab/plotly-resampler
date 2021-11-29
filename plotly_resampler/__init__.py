bba5c51ca62a9563f3aae45e8fe836133ef53b64
jonas <jonvdrdo.vanderdonckt@ugent.be>
2021-11-29 14:40:36 +0100

    :scroll: enhancing __init__ for docs

--- plotly_resampler/__init__.py
+++ plotly_resampler/__init__.py
@@ -1,2 +1,24 @@
+"""<b>plotly\_resampler</b>: interactive visualiziations of large sequences of data
+
+.. include:: ../docs/pdoc_include/root_documentation.md
+
+
+"""
+
 from .figure_resampler import FigureResampler
 from .downsamplers import LTTB, EveryNthPoint, AggregationDownsampler
+
+__docformat__ = "numpy"
+__author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
+__version__ = "0.1.0"
+
+__pdoc__ = {}
+
+__all__ = [
+    "__version__",
+    "__pdoc__",
+    "FigureResampler",
+    "LTTB",
+    "EveryNthPoint",
+    "AggregationDownsampler",
+]
