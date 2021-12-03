44cf2ab89eaea3d17842cd8981c6fd92ebabd854
jonas <jonvdrdo.vanderdonckt@ugent.be>
2021-12-03 21:04:33 +0100

    :dizzy: adding selenium tests

---
+++ tests/test_figure_resampler_selenium.py
@@ -0,0 +1,46 @@
+import pytest
+from .fr_selenium import FigureResamplerGUITests
+import multiprocessing
+import time
+
+
+def test_basic_gui(driver, example_figure):
+    from pytest_cov.embed import cleanup_on_sigterm
+    cleanup_on_sigterm()
+
+    port = 9012
+    proc = multiprocessing.Process(
+        target=example_figure.show_dash, kwargs=dict(mode="external", port=port)
+    )
+    proc.start()
+    try:
+        time.sleep(1)
+        fr = FigureResamplerGUITests(driver, port=port, headless=False)
+        # box based zooms
+        fr.drag_and_zoom("xy", x0=0.25, x1=0.5, y0=0.25, y1=0.5)
+        fr.drag_and_zoom("x2y2", x0=0.3, x1=0.7, y0=0.1, y1=1)
+        fr.drag_and_zoom("x2y2", x0=0.25, x1=0.5, y0=0.2, y1=0.2)
+
+        # y remains the same - zoom horizontally
+        fr.drag_and_zoom("x2y2", x0=0.25, x1=0.5, y0=0.3, y1=0.3)
+        fr.drag_and_zoom("x3y3", x0=0.4, x1=0.5, y0=0.5, y1=0.5)
+
+        fr.drag_and_zoom("x2y2", x0=0.3, x1=0.7, y0=0.1, y1=1)
+
+        # y remains the same - zom horizontally
+        fr.drag_and_zoom("x3y3", x0=0.4, x1=0.5, y0=0.5, y1=0.5)
+        fr.drag_and_zoom("x3y3", x0=0.95, x1=0.5, y0=0.95, y1=0.95)
+        fr.drag_and_zoom("x3y3", x0=0.05, x1=0.5, y0=0.95, y1=0.95)
+        fr.drag_and_zoom("x2y2", x0=0.05, x1=0.5, y0=0.95, y1=0.95)
+        # scale vertically
+        fr.drag_and_zoom("x2y2", x0=0.01, x1=0.01, y0=0.01, y1=0.8)
+
+        fr.autoscale()
+        time.sleep(0.5)
+
+        fr.reset_axes()
+        time.sleep(0.5)
+    except:
+        raise Exception
+    finally:
+        proc.terminate()
