840d21f2225baec5ebd4b232b088b0f1e47d5644
jonas <jonvdrdo.vanderdonckt@ugent.be>
2022-01-13 14:54:16 +0100

    :goat: more selenium network tests

--- tests/fr_selenium.py
+++ tests/fr_selenium.py
@@ -7,9 +7,11 @@ Selenium wrapper class withholding methods for testing the plolty-figureResample
 
 """
 
+from __future__ import annotations
+
 __author__ = "Jonas Van Der Donckt"
 
-from os import stat
+import json
 import time
 from datetime import datetime, timedelta
 from typing import List, Union
@@ -32,7 +34,7 @@ class RequestParser:
     def filter_callback_requests(requests: List[Request]) -> List[Request]:
         valid_requests = []
         for r in requests:
-            if r.method.upper() != 'POST':
+            if r.method.upper() != "POST":
                 # note; the `_reload_hash` GET request will thus be filtered out
                 continue
 
@@ -41,6 +43,107 @@ class RequestParser:
 
             valid_requests.append(r)
         return valid_requests
+
+    def assert_fetch_data_request(
+        data_request: Request, relayout_keys: List[str], n_updated_traces: int
+    ):
+        """Withholds checks for the relayout-data fetch request
+
+        Parameters
+        ----------
+        data_request : Request
+            The relayout data fetch request, with
+            * Request body: the relayout changes
+            * Response body: a list of dicts with first tiem
+        relayout_keys : List[str]
+            The expected keys to be found in the relayout command
+        n_updated_traces : int
+            The expected amount of traces which will be updated.
+
+        """
+        fetch_data_body = json.loads(data_request.body)
+        assert "inputs" in fetch_data_body and len(fetch_data_body["inputs"]) == 1
+        assert fetch_data_body["inputs"][0]["id"] == "resample-figure"
+        assert fetch_data_body["inputs"][0]["property"] == "relayoutData"
+        assert all(k in fetch_data_body["inputs"][0]["value"] for k in relayout_keys)
+        fetch_data_response_body = json.loads(data_request.response.body)["response"]
+        updateData = fetch_data_response_body["trace-updater"]["updateData"]
+        # in this case; the length of updateData is 2 as
+        # (1) only the sin-wave is updated ÁND (2) relayout data is always sent first
+        assert len(updateData) == 1 + n_updated_traces
+        # verify that the layout update does not contain trace props
+        assert "x" not in updateData[0]
+        assert "y" not in updateData[0]
+
+        # verify that the trace-update does not contain layout update props
+        for i in range(1, n_updated_traces + 1):
+            assert "x" in updateData[i]
+            assert "y" in updateData[i]
+            # As identifier, we always send the trace-index
+            # (i.e. the the position of the trace in the `trace-list`)
+            assert "index" in updateData[i]
+
+    def assert_front_end_relayout_request(relayout_request: Request):
+        relayout_body = json.loads(relayout_request.body)
+        assert "inputs" in relayout_body and len(relayout_body["inputs"]) == 1
+        assert relayout_body["inputs"][0]["id"] == "resample-figure"
+        assert relayout_body["inputs"][0]["property"] == "relayoutData"
+        assert all(
+            k in relayout_body["inputs"][0]["value"]
+            for k in ["annotations", "template", "title", "legend", "xaxis", "yaxis"]
+        )
+
+        relayout_response_body = json.loads(relayout_request.response.body)["response"]
+        # the relayout response its updateData should be an empty dict
+        # { "response": { "trace-updater": { "updateData": [ {} ] } } }
+        updateData = relayout_response_body["trace-updater"]["updateData"]
+        assert len(updateData) == 1
+        assert updateData[0] == {}
+
+    def browser_independent_single_callback_request_assert(
+        fr: FigureResamplerGUITests, relayout_keys: List[str], n_updated_traces: int
+    ):
+        """Verifies the callback requests on a browser-independent manner
+
+        fr: FigureResamplerGUITests
+            used for determining the browser-type.
+        requests: List[Request]
+            The captured requests of a SINGLE INTERACTION CALLBACK
+        relayout_keys : List[str]
+            The expected keys to be found in the relayout command
+        n_updated_traces : int
+            The expected amount of traces which will be updated.
+
+        """
+        # First, filter the requests to only retain the relevant ones
+        requests = RequestParser.filter_callback_requests(fr.get_requests())
+
+        browser_name = fr.driver.capabilities["browserName"]
+        if browser_name == "firefox":
+            # There are 2 requests which are send
+            # 1. first: changed-layout to server -> new data to back-end request
+            # 2. the front-end relayout request
+            assert len(requests) >= 1
+            if len(requests) == 2:
+                fetch_data_request, relayout_request = requests
+                RequestParser.assert_front_end_relayout_request(relayout_request)
+            else:
+                fetch_data_request = requests[0]
+
+        elif browser_name == "chrome":
+            # for some, yet unknown reason, chrome does not seem to capture the
+            # second front-end request.
+            assert len(requests) == 1
+            fetch_data_request = requests[0]
+        else:
+            raise ValueError(f"invalid browser name {browser_name}")
+
+        # Validate the update-data-callback request
+        RequestParser.assert_fetch_data_request(
+            fetch_data_request,
+            relayout_keys=relayout_keys,
+            n_updated_traces=n_updated_traces,
+        )
 
 
 class FigureResamplerGUITests:
@@ -135,8 +238,9 @@ class FigureResamplerGUITests:
         actions = ActionChains(self.driver)
         actions.move_to_element_with_offset(subplot, xoffset=w * x0, yoffset=h * y0)
         actions.click_and_hold()
+        actions.pause(0.2)
         actions.move_by_offset(xoffset=w * (x1 - x0), yoffset=h * (y1 - y0))
-        actions.pause(0.1)
+        actions.pause(0.2)
         actions.release()
         actions.pause(0.2)
         actions.perform()
--- tests/test_figure_resampler_selenium.py
+++ tests/test_figure_resampler_selenium.py
@@ -33,57 +33,13 @@ def test_basic_example_gui(driver, example_figure):
         # Perform a zoom operation, and capture the request output
         fr.drag_and_zoom("x2y2", x0=0.25, x1=0.5, y0=0.2, y1=0.2)
         time.sleep(1)
-        requests = RequestParser.filter_callback_requests(fr.get_requests())
-        # There are 2 requests which are send
-        # 1. first: changed-layout to server -> new data to back-end request
-        # 2. the front-end relayout request
-        assert len(requests) == 2
-        fetch_data_request, relayout_request = requests
         # 1. Verify the fetch data request
-        fetch_data_body = json.loads(fetch_data_request.body)
-        assert "inputs" in fetch_data_body and len(fetch_data_body["inputs"]) == 1
-        assert fetch_data_body["inputs"][0]["id"] == "resample-figure"
-        assert fetch_data_body["inputs"][0]["property"] == "relayoutData"
-        assert all(
-            k in fetch_data_body["inputs"][0]["value"]
-            for k in ["xaxis2.range[0]", "xaxis2.range[1]"]
-        )
-        fetch_data_response_body = json.loads(fetch_data_request.response.body)[
-            "response"
-        ]
-        updateData = fetch_data_response_body["trace-updater"]["updateData"]
-        # in this case; the length of updateData is 2 as
-        # (1) only the sin-wave is updated ÁND (2) relayout data is always sent first
-        assert len(updateData) == 2
-        # verify that the layout update does not contain trace props
-        assert "x" not in updateData[0]
-        assert "y" not in updateData[0]
-
-        # verify that the trace-update does not contain layout update props
-        assert "x" in updateData[1]
-        assert "y" in updateData[1]
-        # As identifier, we always send the trace-index
-        # (i.e. the the position of the trace in the `trace-list`)
-        assert "index" in updateData[1]
-
-        # 2. Verify the relayout request
-        relayout_body = json.loads(relayout_request.body)
-        assert "inputs" in relayout_body and len(relayout_body["inputs"]) == 1
-        assert relayout_body["inputs"][0]["id"] == "resample-figure"
-        assert relayout_body["inputs"][0]["property"] == "relayoutData"
-        assert all(
-            k in relayout_body["inputs"][0]["value"]
-            for k in ["annotations", "template", "title", "legend", "xaxis", "yaxis"]
+        RequestParser.browser_independent_single_callback_request_assert(
+            fr, relayout_keys=["xaxis2.range[0]", "xaxis2.range[1]"], n_updated_traces=1
         )
 
-        relayout_response_body = json.loads(relayout_request.response.body)["response"]
-        # the relayout response its updateData should be an empty dict
-        # { "response": { "trace-updater": { "updateData": [ {} ] } } }
-        updateData = relayout_response_body["trace-updater"]["updateData"]
-        assert len(updateData) == 1
-        assert updateData[0] == {}
-
-        # just a legend toggle operation, no new requests should be made
+        # A legend toggle operation
+        # This does not trigger the relayout callback, no new requests should be made
         fr.clear_requests()
         fr.click_legend_item("room 3")
         time.sleep(1)
@@ -91,26 +47,62 @@ def test_basic_example_gui(driver, example_figure):
 
         # y remains the same - zoom horizontally
         fr.drag_and_zoom("x2y2", x0=0.25, x1=0.5, y0=0.3, y1=0.3)
+
+        fr.clear_requests()
         fr.drag_and_zoom("x3y3", x0=0.4, x1=0.5, y0=0.5, y1=0.5)
+        time.sleep(1)
+        RequestParser.browser_independent_single_callback_request_assert(
+            fr, relayout_keys=["xaxis3.range[0]", "xaxis3.range[1]"], n_updated_traces=3
+        )
 
         fr.drag_and_zoom("x2y2", x0=0.3, x1=0.7, y0=0.1, y1=1)
 
-        # y remains the same - zom horizontally
+        # y remains the same - zoom horizontally
         fr.click_legend_item("room 3")
         fr.click_legend_item("room 2")
         fr.drag_and_zoom("x3y3", x0=0.4, x1=0.5, y0=0.5, y1=0.5)
         fr.drag_and_zoom("x3y3", x0=0.95, x1=0.5, y0=0.95, y1=0.95)
         fr.drag_and_zoom("x3y3", x0=0.05, x1=0.5, y0=0.95, y1=0.95)
         fr.drag_and_zoom("x2y2", x0=0.05, x1=0.5, y0=0.95, y1=0.95)
-        # scale vertically
-        fr.drag_and_zoom("x2y2", x0=0.01, x1=0.01, y0=0.01, y1=0.8)
 
-        # check whether the autoscale command updates the data
+        # scale vertically -
+        # This will trigger a relayout callback, however as only y-values are updated,
+        # no new data points will be send to the front-end and a - NO CONTENT response
+        # will be returned.
+        fr.clear_requests()
+        fr.drag_and_zoom("x2y2", x0=0.5, x1=0.5, y0=0.1, y1=0.5)
+        time.sleep(1)
+        vertical_requests = RequestParser.filter_callback_requests(fr.get_requests())
+        assert len(vertical_requests) == 1
+        assert vertical_requests[0].response.status_code == 204
+
+        # we autoscale to the current front-end view, no updated dat will be sent from
+        # the server to the front-end, however, a callback will still be made, but
+        # will return a 204 response status (no content), as we do not need new data
+        # to autoscale to the current front-end view.
+        fr.clear_requests()
         fr.autoscale()
-        time.sleep(0.2)
+        time.sleep(1)
+        autoscale_requests = RequestParser.filter_callback_requests(fr.get_requests())
+        assert len(autoscale_requests) == 1
+        assert autoscale_requests[0].response.status_code == 204
 
+        # The reset axes autoscales AND resets tot he global data view -> all data
+        # will be updated.
+        fr.clear_requests()
         fr.reset_axes()
-        time.sleep(0.2)
+        time.sleep(1)
+        RequestParser.browser_independent_single_callback_request_assert(
+            fr,
+            relayout_keys=[
+                "xaxis.autorange",
+                "xaxis2.autorange",
+                "xaxis3.autorange",
+                "xaxis.showspikes",
+            ],
+            n_updated_traces=5,
+        )
+
     except Exception as e:
         raise e
     finally:
@@ -135,26 +127,79 @@ def test_gsr_gui(driver, gsr_figure):
         # box based zooms
         fr.drag_and_zoom("xy", x0=0.25, x1=0.5, y0=0.25, y1=0.5)
         fr.drag_and_zoom("x2y3", x0=0.3, x1=0.7, y0=0.1, y1=1)
-        fr.drag_and_zoom("x2y3", x0=0.25, x1=0.5, y0=0.2, y1=0.2)
 
+        # Note: we have shared-xaxes so all traces will be updated using this command
+        fr.clear_requests()
+        fr.drag_and_zoom("x2y3", x0=0.25, x1=0.5, y0=0.2, y1=0.2)
+        RequestParser.browser_independent_single_callback_request_assert(
+            fr=fr,
+            relayout_keys=[
+                "xaxis.range[0]",
+                "xaxis.range[1]",
+                "xaxis2.range[0]",
+                "xaxis2.range[1]",
+            ],
+            n_updated_traces=7,
+        )
+
+        # A toggle operation
+        # This does not trigger the relayout callback, no new requests should be made
+        fr.clear_requests()
         fr.click_legend_item("EDA_Phasic")
         time.sleep(0.1)
         fr.click_legend_item("SCR peaks")
+        time.sleep(1)
+        assert len(RequestParser.filter_callback_requests(fr.get_requests())) == 0
+
+        # A reset axes operation resets the front-end view to the global data view
+        fr.clear_requests()
         fr.reset_axes()
+        time.sleep(1)
+        RequestParser.browser_independent_single_callback_request_assert(
+            fr=fr,
+            relayout_keys=[
+                "xaxis.autorange",
+                "xaxis2.autorange",
+                "xaxis.showspikes",
+                "xaxis2.showspikes",
+            ],
+            n_updated_traces=7,
+        )
 
         # y remains the same - zoom horizontally
+        fr.clear_requests()
         fr.drag_and_zoom("x2y3", x0=0.25, x1=0.5, y0=0.3, y1=0.3)
 
         # y remains the same - zom horizontally
         fr.drag_and_zoom("xy", x0=0.4, x1=0.5, y0=0.5, y1=0.5)
         fr.drag_and_zoom("x2y3", x0=0.95, x1=0.5, y0=0.95, y1=0.95)
-        fr.click_legend_item('EDA_lf_cleaned_tonic')
+        fr.click_legend_item("EDA_lf_cleaned_tonic")
         fr.drag_and_zoom("xy", x0=0.05, x1=0.5, y0=0.95, y1=0.95)
         fr.drag_and_zoom("x2y3", x0=0.05, x1=0.5, y0=0.95, y1=0.95)
+
         # scale vertically
-        fr.drag_and_zoom("x2y3", x0=0.01, x1=0.01, y0=0.01, y1=0.8)
+        # This will trigger a relayout callback, however as only y-values are updated,
+        # no new data points will be send to the front-end and a - NO CONTENT response
+        # will be returned.
+        fr.clear_requests()
+        fr.drag_and_zoom("x2y3", x0=0.2, x1=0.2, y0=0.1, y1=0.5)
+        time.sleep(1)
+        vertical_requests = RequestParser.filter_callback_requests(fr.get_requests())
+        assert len(vertical_requests) == 1
+        assert vertical_requests[0].response.status_code == 204
+        
+        # autoscale
+        # we autoscale to the current front-end view, no updated dat will be sent from
+        # the server to the front-end, however, a callback will still be made, but
+        # will return a 204 response status (no content), as we do not need new data
+        # to autoscale to the current front-end view.
+        fr.clear_requests()
         fr.autoscale()
-        time.sleep(0.2)
+        time.sleep(1)
+        autoscale_requests = RequestParser.filter_callback_requests(fr.get_requests())
+        assert len(autoscale_requests) == 1
+        assert autoscale_requests[0].response.status_code == 204
+
 
         fr.reset_axes()
         time.sleep(0.2)
