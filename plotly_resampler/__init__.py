05b252c3320b06ac90b2e29cb29b542fb7071d43
jvdd <boebievdd@gmail.com>
2022-03-11 23:54:54 +0100

    :rocket: new release

--- plotly_resampler/__init__.py
+++ plotly_resampler/__init__.py
@@ -10,7 +10,7 @@ from .downsamplers import LTTB, EveryNthPoint, AggregationDownsampler
 
 __docformat__ = "numpy"
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 
 __pdoc__ = {}
 
