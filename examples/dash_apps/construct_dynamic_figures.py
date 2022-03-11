d6356e6a993d9f8cbcf2d86e8e98d2f2929618a4
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2022-03-11 14:43:25 +0100

    :disguised_face: pattern matching callback dash app!

--- examples/dash_apps/construct_dynamic_figures.py
+++ examples/dash_apps/construct_dynamic_figures.py
@@ -1,48 +1,91 @@
-from dash import Dash, dcc, html, Input, Output, State, MATCH
-import dash_bootstrap_components as dbc
-import numpy as np
-from plotly_resampler import FigureResampler
-import plotly.graph_objects as go
-from trace_updater import TraceUpdater
+from typing import Dict
 from uuid import uuid4
 
+import numpy as np
+import plotly.graph_objects as go
+
+import dash
+import dash_bootstrap_components as dbc
+from dash import Dash, dcc, html, Input, Output, State, MATCH
 from dash.exceptions import PreventUpdate
 
-# The globabl variables
-graph_dict = {}
-app = Dash(__name__, suppress_callback_exceptions=True) # external_stylesheets=[dbc.themes.COSMO]
+from trace_updater import TraceUpdater
+from plotly_resampler import FigureResampler
 
-# Construct the app layout
-app.layout = dbc.Container(
+# The global variables
+graph_dict: Dict[str, FigureResampler] = {}
+app = Dash(
+    __name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX]
+)
+
+# ---------------------------- Construct the app layout ----------------------------
+app.layout = html.Div(
     [
-        dbc.Container(html.H1("Sine graph generator"), style={"textAlign": "center"}),
+        html.Div(html.H1("Exponential sine generator"), style={"textAlign": "center"}),
         html.Hr(),
         dbc.Row(
             [
                 dbc.Col(
-                    [
-                        dcc.Input(id="nbr-datapoints", placeholder="n", type="number"),
-                        dcc.Input(
-                            id="expansion-factor",
-                            placeholder="gamma",
-                            type="number",
-                        ),
-                        html.Button(id="add-graph-btn", children="Add a graph"),
-                    ],
+                    dbc.Form(
+                        [
+                            dbc.Label("#datapoints:", style={"margin-left": "10px"}),
+                            html.Br(),
+                            dcc.Input(
+                                id="nbr-datapoints",
+                                placeholder="n",
+                                type="number",
+                                style={"margin-left": "10px"},
+                            ),
+                            *([html.Br()] * 2),
+                            dbc.Label("exponent:", style={"margin-left": "10px"}),
+                            html.Br(),
+                            dcc.Input(
+                                id="expansion-factor",
+                                placeholder="pow",
+                                type="number",
+                                min=0.95,
+                                max=1.00001,
+                                style={"margin-left": "10px"},
+                            ),
+                            *([html.Br()] * 2),
+                            dbc.Button(
+                                "Create new graph",
+                                id="add-graph-btn",
+                                color="primary",
+                                style={
+                                    "textalign": "center",
+                                    "width": "max-content",
+                                    "margin-left": "10px",
+                                },
+                            ),
+                            *([html.Br()] * 2),
+                            dbc.Button(
+                                "Remove last graph",
+                                id="remove-graph-btn",
+                                color="danger",
+                                style={
+                                    "textalign": "center",
+                                    "width": "max-content",
+                                    "margin-left": "10px",
+                                },
+                            ),
+                        ],
+                    ),
+                    style={"align": "top"},
                     md=2,
                 ),
                 dbc.Col(html.Div(id="graph-container"), md=10),
             ],
-            align="center",
         ),
     ]
 )
 
 
-# ------------------------------- Callbacks --------------------------------------
+# -------------------------------- Callbacks ---------------------------------------
 @app.callback(
     Output("graph-container", "children"),
     Input("add-graph-btn", "n_clicks"),
+    Input("remove-graph-btn", "n_clicks"),
     [
         State("nbr-datapoints", "value"),
         State("expansion-factor", "value"),
@@ -50,39 +93,56 @@ app.layout = dbc.Container(
     ],
     prevent_initial_call=True,
 )
-def add_sin(n_clicks, n, gamma, graph_container):
-    print(f"n_clicks: {n_clicks}\tnbr_datapoints: {n}\texpansion factor:{gamma}")
-    if n_clicks is None or n is None or gamma is None:
+def add_or_remove_graph(add_graph, remove_graph, n, exp, gc):
+    if (add_graph is None or n is None or exp is None) and (remove_graph is None):
         raise PreventUpdate()
 
+    # Transform the graph data to a figure
+    gc = [] if gc is None else gc
+    if len(gc):
+        _gc = []
+        for i in range(len(gc) // 2):
+            _gc.append(dcc.Graph(**gc[i * 2]["props"]))
+            _gc.append(
+                TraceUpdater(**{k: gc[i * 2 + 1]["props"][k] for k in ["id", "gdID"]})
+            )
+        gc = _gc
+
+    # Check if we need to remove a graph
+    clicked_btns = [p["prop_id"] for p in dash.callback_context.triggered]
+    if any("remove-graph" in btn_name for btn_name in clicked_btns):
+        if not len(gc):
+            raise PreventUpdate()
+
+        graph_dict.pop(gc[-1].__getattribute__("gdID"))
+        return [*gc[:-2]]
+
+    # No graph needs to be removed -> create a new graph
     x = np.arange(n)
-    expansion_scaling = x ** gamma
-    y = np.sin(x / 10) * expansion_scaling + np.random.randn(n) / 10 * gamma
+    expansion_scaling = exp ** x
+    y = np.sin(x / 10) * expansion_scaling + np.random.randn(n) / 10 * expansion_scaling
 
-    global graph_dict
-    fr = FigureResampler(go.Figure())
-
+    fr = FigureResampler(go.Figure(), verbose=True)
     fr.add_trace(go.Scattergl(name="sin"), hf_x=x, hf_y=y)
     fr.update_layout(
-        height=300,
+        height=350,
         showlegend=True,
-        legend=dict(orientation="h"),
-        title=f"graph {len(graph_dict) + 1} - n={n} epxansion={gamma}",
+        legend=dict(orientation="h", y=1.12, xanchor="right", x=1),
+        template="plotly_white",
+        title=f"graph {len(graph_dict) + 1} - n={n:,} pow={exp}",
         title_x=0.5,
     )
 
-    graph_container = [] if graph_container is None else graph_container
-
+    # Create a uuid for the graph and add it to the global graph dict,
     uid = str(uuid4())
     graph_dict[uid] = fr
-    return html.Div(
-        [
-            html.Div(graph_container),
-            # html.H2(f"graph-{len(graph_dict)}"),
-            dcc.Graph(figure=fr, id={"type": "dynamic-graph", "index": uid}),
-            TraceUpdater(id={"type": "dynamic-updater", "index": uid}, gdID=uid),
-        ]
-    )
+
+    # add the graph to the existing output
+    return [
+        *gc,
+        dcc.Graph(figure=fr, id={"type": "dynamic-graph", "index": uid}),
+        TraceUpdater(id={"type": "dynamic-updater", "index": uid}, gdID=uid),
+    ]
 
 
 # The generic resampling callback
@@ -91,10 +151,10 @@ def add_sin(n_clicks, n, gamma, graph_container):
     Output({"type": "dynamic-updater", "index": MATCH}, "updateData"),
     Input({"type": "dynamic-graph", "index": MATCH}, "relayoutData"),
     State({"type": "dynamic-graph", "index": MATCH}, "id"),
+    prevent_initial_call=True,
 )
-def update_figure(relayoutdata, id):
-    # fetch the corresponding graph and update it.
-    return graph_dict.get(id.get("index"))._update_graph(relayoutdata)
+def update_figure(relayoutdata: dict, graph_id_dict: dict):
+    return graph_dict.get(graph_id_dict.get("index"))._update_graph(relayoutdata)
 
 
 if __name__ == "__main__":
