62fe7684e9cba86c893dde4d7538127ccad54024
Jonas Van Der Donckt <38005924+jonasvdd@users.noreply.github.com>
2022-03-29 15:48:24 +0200

    :see_no_evil:

--- .github/deploy-gh-pages.sh
+++ .github/deploy-gh-pages.sh
@@ -18,7 +18,7 @@ mkdir -p gh-pages/docs/
 cp -R docs/plotly_resampler/* gh-pages/docs/
 cd gh-pages
 touch docs/.nojekyll
-gir rm docs/*
+git rm docs/*
 git add *
 if git diff --staged --quiet; then
   echo "$0: No changes to commit."
