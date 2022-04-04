5b7259e193299c3846a8f59fbe016ccbe92a934d
jvdd <boebievdd@gmail.com>
2022-04-04 22:31:07 +0200

    :rocket: deploy new version

--- plotly_resampler/__init__.py
+++ plotly_resampler/__init__.py
@@ -7,7 +7,7 @@ from .aggregation import LTTB, EveryNthPoint, FuncAggregator
 
 __docformat__ = "numpy"
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "__version__",
