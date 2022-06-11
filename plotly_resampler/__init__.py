52607dce881a0517776450ee9e6fbceb1454dd7f
jonvdrdo <jonvdrdo.vanderdonckt@ugent.be>
2022-06-11 22:54:27 +0200

    :muscle: adding the (un)register functions into __init__

--- plotly_resampler/__init__.py
+++ plotly_resampler/__init__.py
@@ -1,27 +1,23 @@
 """**plotly\_resampler**: visualizing large sequences."""
 
-from .aggregation import (
-    LTTB,
-    EfficientLTTB,
-    EveryNthPoint,
-    FuncAggregator,
-    MinMaxOverlapAggregator,
-)
+from .aggregation import LTTB, EfficientLTTB, EveryNthPoint
 from .figure_resampler import FigureResampler, FigureWidgetResampler
+from .module import register_plotly_resampler, unregister_plotly_resampler
 
 __docformat__ = "numpy"
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
-__version__ = "0.6.4.2"
+__version__ = "0.7.0"
 
 __all__ = [
     "__version__",
     "FigureResampler",
     "FigureWidgetResampler",
     "EfficientLTTB",
-    "MinMaxOverlapAggregator",
     "LTTB",
     "EveryNthPoint",
     "FuncAggregator",
+    "register_plotly_resampler",
+    "unregister_plotly_resampler",
 ]
 
 
