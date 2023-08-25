34266ca90352569396c2332d5c2dc23047aba60d
jonasvdd <jonvdrdo.vanderdonckt@ugent.be>
2023-08-25 11:49:41 +0200

    :mag: code review

--- plotly_resampler/figure_resampler/assets/coarse_fine.js
+++ plotly_resampler/figure_resampler/assets/coarse_fine.js
@@ -1,3 +1,18 @@
+function getGraphDiv(gdID) {
+    let graphDiv = document?.querySelectorAll('div[id*="' + gdID + '"][class*="dash-graph"]');
+    graphDiv = graphDiv?.[0]?.getElementsByClassName("js-plotly-plot")?.[0];
+    if (!_.isElement(graphDiv)) {
+        throw new Error(`Invalid gdID '${gdID}'`);
+    }
+    return graphDiv;
+}
+
+/**
+ *
+ * @param {object} data The data of the graphDiv
+ * @returns {Array} An array containing all the unique axis keys of the graphDiv data
+ *                  [{x: x[ID], y: y[ID]}, {x: x[ID], y: y[ID]}]
+ */
 const getXYAxisKeys = (data) => {
     return _.chain(data)
         .map((obj) => ({ x: obj.xaxis || "x", y: obj.yaxis || "y" }))
@@ -14,6 +29,13 @@ const getAnchorT = (keys, anchor) => {
     return anchorT;
 };
 
+/**
+ * Get the corresponding axis name of the anchors
+ *
+ * @param {object} layout the layout of the graphDiv
+ * @returns {object} An object containing the anchor and its orthogonal axis name e.g.
+ *                  {x[ID]: yaxis[ID], y[ID]: xaxis[ID]}
+ */
 const getLayoutAxisAnchors = (layout) => {
     return Object.assign(
         {},
@@ -26,27 +48,43 @@ const getLayoutAxisAnchors = (layout) => {
     );
 };
 
-function getGraphDiv(gdID) {
-    let graphDiv = document?.querySelectorAll('div[id*="' + gdID + '"][class*="dash-graph"]');
-    graphDiv = graphDiv?.[0]?.getElementsByClassName("js-plotly-plot")?.[0];
-    if (!_.isElement(graphDiv)) {
-        throw new Error(`Invalid gdID '${gdID}'`);
-    }
-    return graphDiv;
+/**
+ * Compare the equality of two arrays with a certain decimal point presiction
+ * @param {*} objValueArr An array with numeric values
+ * @param {*} othValueArr An array with numeray values
+ * @returns {boolean} true when all values are equal (to 5 decimal points)
+ */
+function rangeCustomizer(objValueArr, othValueArr) {
+    return _.every(
+        _.zipWith(objValueArr, othValueArr, (objValue, othValue) => {
+            if (_.isNumber(objValue) && _.isNumber(othValue)) {
+                objValue = _.round(objValue, 5);
+                othValue = _.round(othValue, 5);
+                return objValue === othValue;
+            } else {
+                alert(`not a number  ${objValue} type:${typeof objValue} | ${othValue} type:${typeof othValue}`);
+            }
+        })
+    );
 }
 
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         coarse_to_main: function (selectedData, mainFigID, coarseFigID) {
-            /* ------------ CALLBACK ----------- */
+            // Base case
+            if (!selectedData.range) {
+                return mainFigID;
+            }
+
             main_graphDiv = getGraphDiv(mainFigID);
             coarse_graphDiv = getGraphDiv(coarseFigID);
 
-            // An object containing each layout axis its x and y anchor {x<ID>: xaxis<ID>, y<ID>: yaxis<ID>}
             const coarse_xy_axiskeys = getXYAxisKeys(coarse_graphDiv.data);
             const main_xy_axiskeys = getXYAxisKeys(main_graphDiv.data);
             const layout_axis_anchors = getLayoutAxisAnchors(main_graphDiv.layout);
 
+            // Use the maingraphDiv its layout to obtain a list of a list of all shared axis names
+            // e.g.: [ [xaxis1, xaxis2],  [xaxis3, xaxis4] ]
             let shared_axes_list = _.chain(main_graphDiv.layout)
                 .map((value, key) => {
                     if (value.matches) return { anchor: value.matches, match: [key] };
@@ -60,10 +98,8 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                             if (_.isArray(objValue)) return objValue.concat(srcValue);
                         });
                     })
-                );
-            // add the axis string to the match array and return the match array
-            // console.log("shared axes list", shared_axes_list);
-            shared_axes_list = shared_axes_list
+                )
+                // add the axis string to the match array and return the match array
                 .map((m_obj) => {
                     const anchorT = getAnchorT(main_xy_axiskeys, m_obj.anchor);
                     let axis_str = layout_axis_anchors[anchorT];
@@ -71,68 +107,52 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                     return m_obj.match;
                 })
                 .value();
-            // console.log("shared axes list", shared_axes_list);
-
-            // De x & y anchors van de coarse graphdiv gebruken?
-            // console.log("selected data", selectedData);
-            // console.log("layout axis anchors", layout_axis_anchors);
-            // console.log("coarse xy axis keys", layout_axis_anchors);
-            if (!selectedData.range) {
-                return mainFigID;
-            }
 
             const relayout = {};
-            const setRelayoutRangeValues = (index, values) => {
+
+            // Quick inline function to set the relayout range values
+            const setRelayoutRangeValues = (axisStr, values) => {
                 for (let i = 0; i < 2; i++) {
-                    relayout[index + `.range[${i}]`] = values[i];
+                    relayout[axisStr + `.range[${i}]`] = values[i];
                 }
             };
+
             // iterate over the selected data range
             for (const anchor_key in selectedData.range) {
                 const selected_range = selectedData.range[anchor_key];
-
-                // NOTE: obtain the anchor key of the other axis (x or y), based on the
-                // coarse graphdiv anchor pairs
+                // Obtain the anchor key of the orthogonal axis (x or y), based on the coarse graphdiv anchor pairs
                 const anchorT = getAnchorT(coarse_xy_axiskeys, anchor_key);
-                const axis_str = layout_axis_anchors[anchorT];
-                const main_layout_range = main_graphDiv.layout[axis_str].range;
+                const axisStr = layout_axis_anchors[anchorT];
+                const mainLayoutRange = main_graphDiv.layout[axisStr].range;
+                const CoarseFigRange = coarse_graphDiv.layout[axisStr].range;
 
-                // console.log("anchor key", anchor_key, "\tanchor T", anchorT, "\taxis str", axis_str);
-                // console.log("axis_range", selected_range);
-                // console.log("main layout range", main_layout_range);
-                // console.log(_.isEqual(selected_range, main_layout_range));
-                if (!_.isEqual(selected_range, main_layout_range)) {
+                if (!_.isEqual(selected_range, mainLayoutRange)) {
                     const shared_axis_match = _.chain(shared_axes_list)
-                        .filter((arr) => arr.includes(axis_str))
+                        .filter((arr) => arr.includes(axisStr))
                         .value()[0];
+                    if (axisStr.includes("yaxis") && _.isEqualWith(selected_range, CoarseFigRange, rangeCustomizer)) {
+                        continue;
+                    }
+
                     if (shared_axis_match) {
-                        shared_axis_match.forEach((axis_m_str) => {
-                            setRelayoutRangeValues(axis_m_str, selected_range);
+                        shared_axis_match.forEach((axisMStr) => {
+                            setRelayoutRangeValues(axisMStr, selected_range);
                         });
                     } else {
-                        setRelayoutRangeValues(axis_str, selected_range);
+                        setRelayoutRangeValues(axisStr, selected_range);
                     }
                 }
             }
 
-            if (Object.keys(relayout).length > 0) {
-                Plotly.relayout(main_graphDiv, relayout);
-            }
+            Object.keys(relayout).length > 0 ? Plotly.relayout(main_graphDiv, relayout) : null;
             return mainFigID;
         },
         main_to_coarse: function (mainRelayout, coarseFigID, mainFigID) {
-            /* ------------ CALLBACK ----------- */
             const coarse_graphDiv = getGraphDiv(coarseFigID);
             const main_graphDiv = getGraphDiv(mainFigID);
-            // console.log("relayout", mainRelayout);
 
-            // An array containing the unique [{x: 'x<ID>', y: 'y<ID>'}] axis keys of the coarse graphDiv trace data
             const coarse_xy_axiskeys = getXYAxisKeys(coarse_graphDiv.data);
-            // console.log("coarse xy axis keys", coarse_xy_axiskeys);
-
-            // An object containing each layout axis its x and y anchor {x<ID>: xaxis<ID>, y<ID>: yaxis<ID>}
             const layout_axis_anchors = getLayoutAxisAnchors(coarse_graphDiv.layout);
-            // console.log("layout axis anchors", layout_axis_anchors);
 
             const currentSelections = coarse_graphDiv.layout.selections;
             const update = { selections: currentSelections || [] };
@@ -142,7 +162,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                     type: "rect",
                     xref: xy_pair.x,
                     yref: xy_pair.y,
-                    line: { width: 1, dash: "dot" },
+                    line: { width: 1, color: "#352F44", dash: "solid" },
                     x0: x_range[0],
                     x1: x_range[1],
                     y0: y_range[0],
@@ -153,14 +173,12 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
             // Base case; no selections yet on the coarse graph
             if (!currentSelections) {
                 // if current selections is None
-                console.log("no selections found");
                 coarse_xy_axiskeys.forEach((xy_pair) => {
                     const x_axis_key = layout_axis_anchors[xy_pair.y];
                     const y_axis_key = layout_axis_anchors[xy_pair.x];
                     const x_range = main_graphDiv.layout[x_axis_key].range;
                     const y_range = main_graphDiv.layout[y_axis_key].range;
 
-                    // console.log(x_range, y_range);
                     update["selections"].push(getUpdateObj(xy_pair, x_range, y_range));
                 });
                 Plotly.relayout(coarse_graphDiv, update);
@@ -174,10 +192,13 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                 const xy_pair = coarse_xy_axiskeys[i];
                 const x_axis_key = layout_axis_anchors[xy_pair.y];
                 const y_axis_key = layout_axis_anchors[xy_pair.x];
-                // console.log(x_axis_key, y_axis_key);
 
                 let x_range = main_graphDiv.layout[x_axis_key].range;
                 let y_range = main_graphDiv.layout[y_axis_key].range;
+                // If the y-axis autorange is true, we alter the y-range to the coarse graphdiv its y-range
+                if (main_graphDiv.layout[y_axis_key]["autorange"] === true) {
+                    y_range = coarse_graphDiv.layout[y_axis_key].range;
+                }
                 if (
                     mainRelayout[x_axis_key + ".autorange"] === true &&
                     mainRelayout[y_axis_key + ".autorange"] === true
@@ -189,17 +210,15 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                     ) {
                         // reset axis -> we use the coarse graphDiv layout
                         x_range = coarse_graphDiv.layout[x_axis_key].range;
-                        y_range = coarse_graphDiv.layout[y_axis_key].range;
                     }
                 } else if (mainRelayout[x_axis_key + ".range[0]"] || mainRelayout[y_axis_key + ".range[0]"]) {
+                    // a specific range is set
                     performed_update = true;
                 }
+
                 update["selections"][i] = getUpdateObj(xy_pair, x_range, y_range);
             }
-
-            if (performed_update) {
-                Plotly.relayout(coarse_graphDiv, update);
-            }
+            performed_update ? Plotly.relayout(coarse_graphDiv, update) : null;
             return coarseFigID;
         },
     },
