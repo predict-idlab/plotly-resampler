5e184d61709e0b246740566d7fecca3e3281e295
jonasvdd <jonvdrdo.vanderdonckt@ugent.be>
2023-11-20 12:02:14 +0100

    :see_no_evil: skip dtype test on window

--- tests/test_figure_resampler.py
+++ tests/test_figure_resampler.py
@@ -2,10 +2,10 @@
 
 __author__ = "Jonas Van Der Donckt, Jeroen Van Der Donckt, Emiel Deprost"
 
-
 import datetime
 import multiprocessing
 import subprocess
+import sys
 import time
 from datetime import timedelta
 from typing import List
@@ -129,6 +129,10 @@ def test_add_trace_not_resampling_insert_gaps():
 
 
 def test_various_dtypes(float_series):
+    # skip the test on windows
+    if sys.platform.lower().startswith("win"):
+        pytest.skip("Skipping test on windows")
+
     # List of dtypes supported by orjson >= 3.8
     valid_dtype_list = [
         np.bool_,
