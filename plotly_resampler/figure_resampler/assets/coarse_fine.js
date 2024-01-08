b945dfbed40cd2347d6f076db3f81c414449193e
Jeroen Van Der Donckt <boebievdd@gmail.com>
2024-01-08 10:02:38 +0100

    :broom: review code

--- plotly_resampler/figure_resampler/assets/coarse_fine.js
+++ plotly_resampler/figure_resampler/assets/coarse_fine.js
@@ -181,8 +181,6 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                 };
             };
 
-            // console.log("main to coarse", mainRelayout);
-
             // Base case; no selections yet on the coarse graph
             if (!currentSelections) {
                 // if current selections is None
